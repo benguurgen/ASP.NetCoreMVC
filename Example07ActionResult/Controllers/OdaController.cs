e424109e91a00928977ef7b527ca0e0ce0929cb8
Bengü Ürgen Demir <benguurgen@gmail.com>
2024-05-28 22:11:29 +0300

    -Action Results

---
+++ Example07ActionResult/Controllers/OdaController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Example07ActionResult.Controllers
+{
+    public class OdaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(IFormCollection form)
+        {
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ Example07ActionResult/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Example07ActionResult.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
